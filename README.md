608bd99b53098bdd260d334f9b99cb2e5bad01fe
cryptidcodes <jrkenny5@gmail.com>
2024-08-27 03:48:54 -0700

    Created player sprite

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # work in progress asteroids pygame project
 pygame 2.6.0 required.
 to setup, run: python3 -m venv venv
+then: source venv/bin/activate
 then run: pip install -r requirements.txt
--- main.py
+++ main.py
@@ -4,19 +4,28 @@
 import pygame
 
 from constants import *
-
+from player import Player
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+    
 
 
 if __name__ == "__main__":
